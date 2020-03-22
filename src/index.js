294d0c3db346d989f29a93a7b392fd793f018cfa
Vinicius <vinicius@localhost.localdomain>
2020-03-22 00:55:27 -0300

    create watson service and integrating in socket

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
+import watson from '~/services/watson';
+import TicketIntentAction from '~/IntentActions/TicketIntentAction';
 
+const server = http.createServer(express());
+const io = socketIO(http);
 const port = process.env.PORT || 3000;
 
-io.on('connection', function(socket){
-  socket.on('message', (msg) => {
-    console.log(`msg: ${msg}`);
-    socket.emit('message', 'Ola Mundo');
+io.on('connection', function(socket) {
+  const watsonInstance = watson();
+  watsonInstance.createSession().then(() => {
+    socket.on('message', (text) => {
+      watsonInstance.sendMessage({ text })
+        .then(res => {
+          const output = res.result.output;
+          const intent = output.intents.length === 0
+            ? 'default'
+            : output.intents[0].intent;
+          const action = TicketIntentAction[intent]
+            ? TicketIntentAction[intent]
+            : TicketIntentAction['default'];
+          action(output, socket);
+        });
+    });
   });
-  console.log('user connected');
 });
 
-http.listen(port, function(){
+server.listen(port, function() {
   console.log(`listening on *:${port}`);
 });
